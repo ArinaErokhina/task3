36e09b07ae6ec7e38988868ecc8a061f5d14b7d4
Lu <lokka.rei@yandex.ru>
2022-11-12 21:37:39 +0700

    task3

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+import java.util.Objects;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !(o instanceof Student)) {
+            return false;
+        }
+        Student student = (Student) o;
+        return name.equals(student.name) && age == student.age;
+    }
+
+    public int hashCode(){
+        return Objects.hash(name,age);
+    }
+
+}
